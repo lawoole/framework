88f14465bc37eb706553f441491a4cbd9fd53cd9
You Ming <youming@funcuter.org>
2018-01-23 15:28:12 +0800

    添加请求处理器绑定任务的特性

---
+++ src/Task/BindRequestManager.php
@@ -0,0 +1,61 @@
+<?php
+namespace Lawoole\Task;
+
+use Illuminate\Contracts\Container\Container;
+use Lawoole\Routing\RequestManager;
+
+trait BindRequestManager
+{
+    /**
+     * 请求管理器编号
+     *
+     * @var string
+     */
+    protected $requestManagerId;
+
+    /**
+     * 绑定请求管理器
+     *
+     * @param \Lawoole\Routing\RequestManager $requestManager
+     *
+     * @return $this
+     */
+    public function withRequestManager(RequestManager $requestManager)
+    {
+        $this->setRequestManagerId($requestManager->getId());
+
+        return $this;
+    }
+
+    /**
+     * 设置请求管理器编号
+     *
+     * @param string $requestManagerId
+     */
+    public function setRequestManagerId($requestManagerId)
+    {
+        $this->requestManagerId = $requestManagerId;
+    }
+
+    /**
+     * 获得请求管理器编号
+     *
+     * @return string
+     */
+    public function getRequestManagerId()
+    {
+        return $this->requestManagerId;
+    }
+
+    /**
+     * 获得管理器
+     *
+     * @param \Illuminate\Contracts\Container\Container $container
+     *
+     * @return \Lawoole\Routing\RequestManager
+     */
+    public function getRequestManager(Container $container)
+    {
+        return RequestManager::getManager($container, $this->requestManagerId);
+    }
+}
