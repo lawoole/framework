2ad331c76d55ae2ba6d2fd37c192da011312aa95
You Ming <youming@funcuter.org>
2018-01-24 11:27:28 +0800

    增加服务器默认事件调用

--- src/Swoole/HttpServer.php
+++ src/Swoole/HttpServer.php
@@ -6,6 +6,18 @@ use Swoole\Http\Server as SwooleHttpServer;
 class HttpServer extends Server
 {
     /**
+     * 可用事件回调
+     *
+     * @var array
+     */
+    protected $serverEvents = [
+        'Start', 'Shutdown', 'ManagerStart', 'ManagerStop',
+        'WorkerStart', 'WorkerStop', 'WorkerExit', 'WorkerError',
+        'Task', 'Finish', 'PipeMessage', 'BufferFull', 'BufferEmpty',
+        'Connect', 'Close', 'Receive', 'Packet', 'Request'
+    ];
+
+    /**
      * 创建 Swoole 服务
      *
      * @return \Swoole\Http\Server
@@ -13,5 +25,15 @@ class HttpServer extends Server
     protected function createSwooleServer()
     {
         return new SwooleHttpServer($this->getUnixSock(), 0, SWOOLE_PROCESS, SWOOLE_SOCK_UNIX_STREAM);
+    }
+
+    /**
+     * 注册事件回调
+     */
+    protected function registerRequestCallback()
+    {
+        $this->swooleServer->on('Request', function ($request, $response) {
+            $this->dispatchEvent('Request', $this, $this->serverSocket, $request, $response);
+        });
     }
 }
--- src/Swoole/WebSocketServer.php
+++ src/Swoole/WebSocketServer.php
@@ -6,6 +6,18 @@ use Swoole\WebSocket\Server as SwooleWebSocketServer;
 class WebSocketServer extends HttpServer
 {
     /**
+     * 可用事件回调
+     *
+     * @var array
+     */
+    protected $serverEvents = [
+        'Start', 'Shutdown', 'ManagerStart', 'ManagerStop',
+        'WorkerStart', 'WorkerStop', 'WorkerExit', 'WorkerError',
+        'Task', 'Finish', 'PipeMessage', 'BufferFull', 'BufferEmpty',
+        'Connect', 'Close', 'Receive', 'Packet', 'Request', 'Open', 'Message'
+    ];
+
+    /**
      * 创建 Swoole 服务
      *
      * @return \Swoole\WebSocket\Server
@@ -13,5 +25,25 @@ class WebSocketServer extends HttpServer
     protected function createSwooleServer()
     {
         return new SwooleWebSocketServer($this->getUnixSock(), 0, SWOOLE_PROCESS, SWOOLE_SOCK_UNIX_STREAM);
+    }
+
+    /**
+     * 注册事件回调
+     */
+    protected function registerOpenCallback()
+    {
+        $this->swooleServer->on('Open', function ($server, $request) {
+            $this->dispatchEvent('Open', $this, $this->serverSocket, $request);
+        });
+    }
+
+    /**
+     * 注册事件回调
+     */
+    protected function registerMessageCallback()
+    {
+        $this->swooleServer->on('Message', function ($server, $frame) {
+            $this->dispatchEvent('Message', $this, $this->serverSocket, $frame);
+        });
     }
 }
