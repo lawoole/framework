4b98ba2a85891756e4c5719ca046e4fde2a2e2c5
You Ming <youming@funcuter.org>
2018-01-24 16:58:45 +0800

    添加解析 Json 请求中间件

---
+++ src/Foundation/Http/Middleware/ParseJsonRequest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Lawoole\Foundatioin\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+
+class ParseJsonRequest
+{
+    /**
+     * 解析 Json 请求参数
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param bool $asObject
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle($request, Closure $next, $asObject = false)
+    {
+        $contentType = $request->header('Content-Type');
+
+        if (Str::startsWith($contentType, 'application/json')) {
+            // 解析 Json 字符串
+            $parameters = json_decode($request->getContent(), !$asObject);
+
+            if (JSON_ERROR_NONE !== json_last_error()) {
+                throw new InvalidArgumentException('Json string cannot decoded, error: '.json_last_error_msg());
+            }
+
+            $request->request->replace($parameters);
+        }
+
+        return $next($request);
+    }
+}
