c02e4b22b0c1e6ded96dd432a5b981facf245a0d
You Ming <youming@funcuter.org>
2018-01-26 12:48:47 +0800

    修复终止命令问题

--- src/Server/Commands/ShutdownCommand.php
+++ src/Server/Commands/ShutdownCommand.php
@@ -31,13 +31,13 @@ class ShutdownCommand extends Command
         if (file_exists($runtimeFile)) {
             $data = json_decode(file_get_contents($this->laravel->storagePath('framework/server.runtime')), true);
 
-            $this->input("{$name} server is shutting down.");
+            $this->info("{$name} server is shutting down.");
 
             Process::kill($data['pid']);
 
             return;
         }
 
-        $this->input("No {$name} server is running.");
+        $this->info("No {$name} server is running.");
     }
 }
