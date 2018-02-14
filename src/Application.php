c8522aab42ce1ef375a31ff5e5deacd8eff2a08a
You Ming <youming@funcuter.org>
2018-02-14 10:23:38 +0800

    更新版本号

--- src/Application.php
+++ src/Application.php
@@ -12,7 +12,7 @@ class Application extends Container implements ApplicationInterface
     /**
      * 框架版本号
      */
-    const VERSION = '0.1.8';
+    const VERSION = '0.1.9';
 
     /**
      * 项目基础路径
