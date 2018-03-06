5b4bc106e349330242f9836a40c14b1a47f28468
You Ming <youming@funcuter.org>
2018-03-06 14:12:52 +0800

    Updated developing version.

--- src/Application.php
+++ src/Application.php
@@ -14,7 +14,7 @@ class Application extends Container implements ApplicationContract
     /**
      * 框架版本号
      */
-    const VERSION = '0.2.0';
+    const VERSION = '0.2.1';
 
     /**
      * 项目基础路径
