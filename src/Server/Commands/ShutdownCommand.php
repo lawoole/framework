173cc7d63e7f1fecfa8b3f0cc7f96ba830bd70b4
You Ming <youming@funcuter.org>
2018-03-14 17:51:52 +0800

    修复服务运行时信息记录文件问题

--- src/Server/Commands/ShutdownCommand.php
+++ src/Server/Commands/ShutdownCommand.php
@@ -26,18 +26,19 @@ class ShutdownCommand extends Command
     public function handle()
     {
         $name = $this->laravel->name();
+
         $runtimeFile = $this->laravel->storagePath('framework/server.runtime');
 
         if (file_exists($runtimeFile)) {
-            $data = json_decode(file_get_contents($this->laravel->storagePath('framework/server.runtime')), true);
+            $payload = json_decode(file_get_contents($this->laravel->storagePath('framework/server.runtime')), true);
 
             $this->info("{$name} server is shutting down.");
 
-            Process::kill($data['pid']);
+            Process::kill($payload['pid']);
 
             return;
         }
 
-        $this->info("No {$name} server is running.");
+        $this->info("{$name} server is not running.");
     }
 }
