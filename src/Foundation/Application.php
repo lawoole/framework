aca26499724c8f4dccd86b364cf13ed3f564d760
You Ming <youming@funcuter.org>
2019-01-04 15:58:08 +0800

    Update version.

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -13,7 +13,7 @@ class Application extends BaseApplication implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.2';
 
     /**
      * The name of service.
